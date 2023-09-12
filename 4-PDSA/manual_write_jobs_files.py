da53f559866057d92ee7518b6df521b0636e7d9c
FredericSabot <frederic.sabot@ulb.be>
2023-09-12 07:43:48 +0200

    PDSA: add script to manually rerun specific jobs
    Signed-off-by: FredericSabot <frederic.sabot@ulb.be>

---
+++ 4-PDSA/manual_write_jobs_files.py
@@ -0,0 +1,25 @@
+from job import Job, SpecialJob
+import sys
+from contingencies import Contingency
+
+def write_jobs_files(args):
+    static_id = int(sys.argv[1])
+    dynamic_seed = int(sys.argv[2])
+    contingency_id = sys.argv[3]
+
+    contingencies = Contingency.create_base_contingency() + Contingency.create_N_1_contingencies() + Contingency.create_N_2_contingencies()
+
+    for contingency in contingencies:
+        if contingency.id == contingency_id:
+            if dynamic_seed == 0:
+                job = SpecialJob(static_id, dynamic_seed, contingency)
+            else:
+                job = Job(static_id, dynamic_seed, contingency)
+            job.write_job_files()
+            return
+    for contingency in contingencies:
+        print(contingency.id)
+    raise ValueError("Contingency {} not found".format(contingency_id))
+
+if __name__ == "__main__":
+    write_jobs_files(sys.argv)
