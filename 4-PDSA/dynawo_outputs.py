Fix missing factor 100 in cost estimation and update fault statistics
Signed-off-by: FredericSabot <frederic.sabot@ulb.be>

4-PDSA/dynawo_outputs.py
@@ -222,7 +222,7 @@ def load_shedding_to_cost(load_shedding, total_load):
             return interp1d(time, cost)(time[0])
 
     H = 0.1419 * load_shedding + 0.6482
-    load_shedding_MW = load_shedding * total_load
+    load_shedding_MW = load_shedding / 100 * total_load
 
     k = 3
     return scipy.integrate.quad(lambda t: k/H*np.exp(-k*t/H) * t * interpolVoLL(t), 0, H, epsabs=1e-4, epsrel=1e-4)[0] * load_shedding_MW / 1e6  # To Millions of euros/dollars